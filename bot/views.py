b8c20bfab7727e5887b26cc01b53a97a710061e2
DevJoymanu <joymanu49@gmail.com>
2025-10-21 14:41:27 +0200

    template_tab_5

--- bot/views.py
+++ bot/views.py
@@ -54,6 +54,7 @@ from django.urls import reverse, reverse_lazy
 from django.db.models import Q
 from .decorators import staff_required
 from django.utils.decorators import method_decorator
+from django.views.decorators.http import require_GET
 
 
 
