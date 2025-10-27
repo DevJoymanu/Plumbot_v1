0416c1f0007c5d97ca49983a7047e158a2b2a0b4
DevJoymanu <joymanu49@gmail.com>
2025-10-27 07:32:42 +0200

    logo_1

--- Plumbing_CRM/settings.py
+++ Plumbing_CRM/settings.py
@@ -9,9 +9,6 @@ import dj_database_url
 
 
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -177,6 +174,10 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
